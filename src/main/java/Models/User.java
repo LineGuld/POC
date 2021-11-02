b5ccf74ec470517fcb1394527e6e8f2e9d832258
Line Guld <linekguld@hotmail.com>
2021-11-02 11:21:49 +0100

    Mapper

--- src/main/java/Models/User.java
+++ src/main/java/Models/User.java
@@ -1,13 +1,22 @@
 package Models;
 
-
 @Entity public class User
 {
   private String username;
+  private int userId;
 
   public User(String username)
   {
     this.username = username;
+  }
+
+  public User()
+  {
+  }
+
+  public void setUserId(int userId)
+  {
+    this.userId = userId;
   }
 
   public String getUsername()
